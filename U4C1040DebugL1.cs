24fe1e7a53ed9474d92898c54c03033c4b900193
Gabe Escobedo <escobedo.gabe@yahoo.com>
2016-06-24 22:26:01 -0500

    someNumber has three different values

---
+++ U4C1040DebugL1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U4C1040DebugL1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var someNumber = 333.5;
+            someNumber = Math.Log(3);
+            someNumber *= Math.PI;
+        }
+    }
+}
